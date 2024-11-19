7cf897f7e99d84346a14637daada2f860b207809
Sleepingotter <141319762+Sleepingotter@users.noreply.github.com>
2024-11-19 21:22:52 +0800

    完成第五次作业2.3

--- 第五次作业.sql
+++ 第五次作业.sql
@@ -1,4 +1,5 @@
 # (1)
+
 use yggl;
 select * from course;
 set @Java_CId := (select CId from course where CName = 'JAVA程序设计');
@@ -22,6 +23,7 @@ DELIMITER ;
 select f_course('C03');
 
 #(3)
+
 delimiter //
 create function exthree(max int,min int) returns int DETERMINISTIC
 begin 
@@ -41,6 +43,7 @@ select exthree(50,0);
 select exthree(150,50);
 
 #(4)
+
 delimiter //
 create function casetwo(var int) returns char(10) deterministic
 begin
@@ -60,30 +63,59 @@ delimiter ;
 select casetwo(7),casetwo(14),casetwo(1);
 
 #(5)
-delimiter //
 
-create function dj_fn() returns char(10) DETERMINISTIC
+delimiter //
+create function dj_fn() returns char(10) deterministic
 begin 
-	declare grade cursor for select Score from register where SId = 'S002' and CId = 'C01';
 	declare result char(10);
+	declare g double;
+	declare grade cursor for select Score from register where SId = 'S002' and CId = 'C01';
+	open grade;
+	fetch grade into g;
 	case
-			when grade >= 90 
-					then result := '优秀';
-			when grade < 90 and grade >= 80
-					then result := '良好';
-			when grade < 80 and grade >= 70
-					then result := '中等';
-			when grade < 70 and grade >= 60
-					then result := '及格';
+			when g >= 90 
+					then set result := '优秀';
+			when g < 90 and g >= 80
+					then set result := '良好';
+			when g < 80 and g >= 70
+					then set result := '中等';
+			when g < 70 and g >= 60
+					then set result := '及格';
 			else 
-					result := '不及格';
+					set result := '不及格';
 	end case;		
+	close grade;
 	return result;		
 end //
-
 delimiter ;
 
+# drop function dj_fn;
+select dj_fn();
 
+# (1) 
+
+create procedure cn_proc()
+begin 
+	select count(1) as '总人数' from student;
+end;
+call cn_proc();
+
+# (2)
+
+create procedure selectscore (in VSId char(10),in VCId char(10))
+begin 
+	select Score from register where VSId = SId and VCId = CId;
+end;
+call selectscore('S002','C01');
+
+# (3)
+
+create procedure upbirth (in VSName char(10),in VBirth date)
+begin 
+	update student set SBirth = VBirth where VSName = SName; 
+end;
+call upbirth('罗丽','2000-11-22');
+select * from student;
 
 
 
